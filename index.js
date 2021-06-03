432cda25bc08675c1e6c1f53a58df70f19619c96
Nori <loveandfight2186@yahoo.co.jp>
2021-06-02 23:54:13 -0700

    mysql setting in index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'password',
+  database: 'PetPaws'
+})
+
+// app.post('')
 
 app.listen(3001, () => {
   console.log("connect")
