cb886abb18d21db3c8ce80f39f8fbdee29a1b178
Shinya Sato <0113shin.s@gmail.com>
2021-06-10 16:11:25 -0700

    delete unused files

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,26 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
-  const {
-    isLoading,
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
   if (error) {
-    return <div>Oops... {error.message}</div>;
+    return <div>Oops... {error.message}</div>
   }
 
   if (isAuthenticated) {
     return (
       <div>
         Hello {user.name}{' '}
-        <button onClick={() => logout({ returnTo: window.location.origin })}>
-          Log out
-        </button>
+        <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
       </div>
-    );
+    )
   } else {
-    return <button onClick={loginWithRedirect}>Log in</button>;
+    return <button onClick={loginWithRedirect}>Log in</button>
   }
 }
 
-export default App;
+export default App
