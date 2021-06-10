9c8f5cccf1429199f1272cd98f8a380d87031bb6
Nori <loveandfight2186@yahoo.co.jp>
2021-06-10 00:55:32 -0700

    run yarn add @auth0/auth0-react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
