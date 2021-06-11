c583e8bc6be0968dfa7f41a1df1356020dad1719
Shinya Sato <0113shin.s@gmail.com>
2021-06-10 19:43:15 -0700

    feat: set react router in App.js/#9

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
+import Home from './pages/Home'
 
 function App() {
   const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
 
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>
-  }
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Oops... {error.message}</div>
 
   if (isAuthenticated) {
     return (
-      <div>
-        Hello {user.name}{' '}
-        <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
-      </div>
+      <>
+        <div>
+          Hello {user.name}{' '}
+          <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
+        </div>
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </>
     )
   } else {
     return <button onClick={loginWithRedirect}>Log in</button>
