c2eeda8200d6eb989089c7e9ebc7476f69dab7d3
Nori <loveandfight2186@yahoo.co.jp>
2021-06-17 16:55:09 -0700

    create pet profile page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 import Home from './pages/Home'
 import Nutrition from './pages/Nutrition'
+import PetProfile from './pages/PetProfile'
 
 function App() {
   const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/nutrition" component={Nutrition} />
+        <Route path="/pet_profile" component={PetProfile} />
       </Switch>
     </Router>
   )
