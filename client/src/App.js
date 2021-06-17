d0fbcad26a6c3b9afc25bc84a528832c9dc90afc
Shinya Sato <0113shin.s@gmail.com>
2021-06-17 09:50:51 -0700

    feat: create nutrition page file and route/#2

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 import Home from './pages/Home'
+import Nutrition from './pages/Nutrition'
 
 function App() {
   const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Oops... {error.message}</div>
+
+  const Routes = () => (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/nutrition" component={Nutrition} />
+      </Switch>
+    </Router>
+  )
 
   if (isAuthenticated) {
     return (
@@ -16,16 +26,16 @@ function App() {
           Hello {user.name}{' '}
           <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
         </div>
-
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </Router>
+        <Routes />
       </>
     )
   } else {
-    return <button onClick={loginWithRedirect}>Log in</button>
+    return (
+      <>
+        <button onClick={loginWithRedirect}>Log in</button>
+        <Routes />
+      </>
+    )
   }
 }
 
