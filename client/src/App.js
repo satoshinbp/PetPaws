27262d9e8f0b851a36e3e6bc5a9465b97b0a4f60
Nori <loveandfight2186@yahoo.co.jp>
2021-06-17 18:46:01 -0700

    delete router dom from index.js and move auth to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import Home from './pages/Home'
 import Nutrition from './pages/Nutrition'
 import PetProfile from './pages/PetProfile'
@@ -8,12 +9,18 @@ import PetProfile from './pages/PetProfile'
 function App() {
   return (
     <Router>
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/nutrition" component={Nutrition} />
-        <Route path="/pet_profile" component={PetProfile} />
-      </Switch>
+      <Auth0Provider
+        domain = {process.env.REACT_APP_AUTH_DOMAIN}
+        clientId= {process.env.REACT_APP_AUTH_CLIENTID}
+        redirectUri={window.location.origin}
+      >
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/nutrition" component={Nutrition} />
+          <Route path="/pet_profile" component={PetProfile} />
+        </Switch>
+      </Auth0Provider>
     </Router>
   )
 }
