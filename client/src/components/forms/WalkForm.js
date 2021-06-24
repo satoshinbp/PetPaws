5a8c9005d03b4441d5d41738773f42a52024363f
Shinya Sato <0113shin.s@gmail.com>
2021-06-24 13:44:11 -0700

    format all files/#46

--- client/src/components/forms/WalkForm.js
+++ client/src/components/forms/WalkForm.js
@@ -1,92 +1,101 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const WalkForm = ({ onAdd }) => {
-    const [name, setName] = useState(''); 
-    const [date, setDate] = useState(new Date().toISOString().slice(0, 10))
-    const [start, setStart] = useState(''); // option "wet" "dry" "treat"
-    const [end, setEnd] = useState(''); // time of meal
-    const [distance, setDistance] = useState('');
+  const [name, setName] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const [start, setStart] = useState(''); // option "wet" "dry" "treat"
+  const [end, setEnd] = useState(''); // time of meal
+  const [distance, setDistance] = useState('');
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        console.log(`${name} ${date} ${start} ${end} ${distance} `)
+    console.log(`${name} ${date} ${start} ${end} ${distance} `);
 
+    onAdd({ name, date, start, end, distance });
 
-        onAdd({name, date, start, end, distance})
+    setName('');
+    setDate('');
+    setStart('');
+    setEnd('');
+    setDistance('');
+  };
 
-        setName('')
-        setDate('')
-        setStart('')
-        setEnd('')
-        setDistance('')
- 
-    }
-
-    return (
-        <div>
-            <h2>Add New Activity</h2>
-            <form className="meal-form" onSubmit={onSubmit}>
-                <div className='form-control'>
-                    {/* MEAL NAME */}
-                    <label htmlFor="name">Name: </label>
-                    <input type='text'
-                           name='name'
-                           id='activity-name'
-                           value={name}
-                           required
-                           onChange={(e) => setName(e.target.value)}
-                    />   
-                    <br /><br />
-                    {/* DATE */}
-                    <label htmlFor="date">Date: </label>
-                    <input type='date' 
-                           id='date' 
-                           name="activity-date" 
-                           defaultValue={date}
-                           required
-                           onChange={(e) => setDate(e.target.value)}
-                    />
-                    <br /><br />
-                    {/* START TIME */}
-                    <label htmlFor="time">Start Time: </label>
-                    <input type="time" 
-                           id="start-time" 
-                           name="start-time"
-                           value={start} 
-                           required
-                           onChange={(e) => setStart(e.target.value)}
-                    />
-                    <br /><br />
-                    {/* END TIME */}
-                    <label htmlFor="time">End Time: </label>
-                    <input type="time" 
-                           id="end-time" 
-                           name="end-time"
-                           value={end} 
-                           required
-                           onChange={(e) => setEnd(e.target.value)}
-                    />
-                    <br /><br />
-                    {/* Distance */}
-                    <label htmlFor="amount">Distance(m): </label>
-                    <input type="number" 
-                           id="amount"
-                           value={distance}
-                           min="0" 
-                           max="50000" 
-                           onChange={(e) => setDistance(e.target.value)}
-                    /> 
-                    <br /><br />
-                    {/* SUBMIT */}
-                    <input type="submit" value="Add" />
-                    <br /><br />
-                </div>              
-            </form>
-            <Link to="/">Go Back</Link>
+  return (
+    <div>
+      <h2>Add New Activity</h2>
+      <form className="meal-form" onSubmit={onSubmit}>
+        <div className="form-control">
+          {/* MEAL NAME */}
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            id="activity-name"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          {/* DATE */}
+          <label htmlFor="date">Date: </label>
+          <input
+            type="date"
+            id="date"
+            name="activity-date"
+            defaultValue={date}
+            required
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <br />
+          <br />
+          {/* START TIME */}
+          <label htmlFor="time">Start Time: </label>
+          <input
+            type="time"
+            id="start-time"
+            name="start-time"
+            value={start}
+            required
+            onChange={(e) => setStart(e.target.value)}
+          />
+          <br />
+          <br />
+          {/* END TIME */}
+          <label htmlFor="time">End Time: </label>
+          <input
+            type="time"
+            id="end-time"
+            name="end-time"
+            value={end}
+            required
+            onChange={(e) => setEnd(e.target.value)}
+          />
+          <br />
+          <br />
+          {/* Distance */}
+          <label htmlFor="amount">Distance(m): </label>
+          <input
+            type="number"
+            id="amount"
+            value={distance}
+            min="0"
+            max="50000"
+            onChange={(e) => setDistance(e.target.value)}
+          />
+          <br />
+          <br />
+          {/* SUBMIT */}
+          <input type="submit" value="Add" />
+          <br />
+          <br />
         </div>
-    )
-}
+      </form>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+};
 
-export default WalkForm
+export default WalkForm;
--- client/src/pages/CreateMeal.js
+++ client/src/pages/CreateMeal.js
@@ -1,14 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import MealForm from '../components/forms/MealForm';
 
-
-
 export const CreateMeal = () => {
-
-    //const [meals, setMeals] = useState('');
-    const addMeal = async(meal) => {
-        console.log('clicked Submit Button');
-        /* // when connect with database
+  //const [meals, setMeals] = useState('');
+  const addMeal = async (meal) => {
+    console.log('clicked Submit Button');
+    /* // when connect with database
         const res = await fetch('http://localhost:5000/meals', {
             method: 'POST',
             headers: {
@@ -21,14 +18,14 @@ export const CreateMeal = () => {
     
         setMeals([...meals, data])
         */
-    }
+  };
 
-    return (
-        <div>
-            <h2 style={{border: '1px solid black', borderRadius: '5px', backgroundColor: '#F0F0F0'}}>Add a Meal</h2>
-            <MealForm onAdd={addMeal} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2 style={{ border: '1px solid black', borderRadius: '5px', backgroundColor: '#F0F0F0' }}>Add a Meal</h2>
+      <MealForm onAdd={addMeal} />
+    </div>
+  );
+};
 
-export default CreateMeal
+export default CreateMeal;
