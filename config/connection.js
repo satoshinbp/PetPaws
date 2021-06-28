9ff76972e8ea0bad368cd6631f8b67603a44d00d
Nori <loveandfight2186@yahoo.co.jp>
2021-06-28 15:44:03 -0700

    feat: [firebase] npm i firebase-admin and create connection.js for db setting

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+});
+
+module.exports = db;
