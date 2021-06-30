bbefeb6065b8f0c6bb1af2d008d8b87b15eb8dc2
Shinya Sato <0113shin.s@gmail.com>
2021-06-30 14:57:08 -0700

    feat: delete firebase timestamp and unused import/#69

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -1,11 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/functions';
-import 'firebase/storage';
 import { firebaseConfig } from './config';
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-export const FirebaseTimestamp = firebase.firestore.Timestamp;
