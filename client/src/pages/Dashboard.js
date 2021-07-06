9a0d14322bd39aaa967e0423c1f8c65ac8cf15dd
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-07-05 20:04:33 -0700

    modified petDetails page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PetDetails from '../components/PetDetails';
+import Axios from 'axios';
+import { useAuth } from '../contexts/AuthContext';
 
 const Dashboard = () => {
+  const [petDetail, setPetDetail] = useState('');
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/user', { params: { uid: currentUser.uid } }).then((response) => {
+      const fetchPets = async () => {
+        const params = { params: { user_id: response.id } };
+        const res = await fetch(`http://localhost:3001/api/pet/get/${response.data[0].id}`, params);
+        const data = await res.json();
+        return data;
+      };
+
+      const getPets = async () => {
+        const fetchedPets = await fetchPets();
+        console.log('fetchedPets', fetchedPets);
+        setPetDetail(fetchedPets);
+      };
+      getPets();
+    });
+  }, []);
+
   return (
     <div>
       <h2 style={{ border: '1px solid black', borderRadius: '5px', backgroundColor: '#F0F0F0' }}>Dashboard</h2>
       <h3>Pet Details</h3>
-      <PetDetails />
+      <div>
+        {petDetail ? (
+          <div key={petDetail.id}>
+            <p>{petDetail.name} </p>
+            <p>{petDetail.breed}</p>
+          </div>
+        ) : (
+          'Loading...'
+        )}
+      </div>
+      {/* <PetDetails /> */}
       <h3>In Last 7 Days</h3>
       {/* <Link to="/mealsummary" /> */}
       <h3>Nutrition Summary</h3>
@@ -17,5 +51,4 @@ const Dashboard = () => {
     </div>
   );
 };
-
 export default Dashboard;
