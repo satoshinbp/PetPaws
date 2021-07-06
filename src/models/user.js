66fe265b86d80934462ea0a56f595f4055b030a6
Nori <loveandfight2186@yahoo.co.jp>
2021-07-06 16:41:25 -0700

    commented out  lines for no record of user find api

--- src/models/user.js
+++ src/models/user.js
@@ -6,12 +6,17 @@ const User = {};
 User.find = (req, res) => {
   const sql = `SELECT * FROM petpaws.users WHERE uid=?`;
   db.query(sql, req, (err, data) => {
-    if (err) {
-      throw err;
-    } else if (data.length === 1) {
+    // line about 15 may have issue, leave it, solve later
+    // if (err) {
+    //   throw err;
+    // } else if (data.length === 1) {
+    //   res(null, data);
+    // } else {
+    //   console.log('aaaaa');
+    //   res(data);
+    // }
+    if (data.length === 1) {
       res(null, data);
-    } else {
-      res.status(404).send('Could not find a user');
     }
   });
 };
