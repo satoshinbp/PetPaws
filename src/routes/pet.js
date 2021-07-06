c1599fffdd5c9fa1946a9188de6f73a38cc9078b
Shinya Sato <0113shin.s@gmail.com>
2021-07-05 20:07:40 -0700

    backend pet route modification

--- src/routes/pet.js
+++ src/routes/pet.js
@@ -1,6 +1,5 @@
 const petController = require('../controller/pet');
 
 module.exports = (app) => {
-  // Retrieve all Stores
-  app.get('/api/pet/get/:user_id', petController.findOne);
+  app.get('/api/pet/get', petController.findAll);
 };
