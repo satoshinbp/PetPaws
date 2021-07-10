9d8ce7e9a13fcd524bce2cac04aa67499e1102da
Shinya Sato <0113shin.s@gmail.com>
2021-07-10 08:44:55 -0700

    feat: add api endpoint for pet profile update

--- src/routes/pet.js
+++ src/routes/pet.js
@@ -3,4 +3,5 @@ const petController = require('../controller/pet');
 module.exports = (app) => {
   app.get('/api/pet', petController.findAll);
   app.post('/api/pet', petController.create);
+  app.post('/api/pet/:id', petController.update);
 };
