7c19a4b3604c7463f21119f5a75b9fc05e249a43
Shinya Sato <0113shin.s@gmail.com>
2021-07-11 11:42:38 -0700

    delete console.log

--- client/src/components/charts/WalkMonthChart.js
+++ client/src/components/charts/WalkMonthChart.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { ResponsiveContainer, ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Area } from 'recharts';
 
 const WalkMonthChart = ({ allActivities }) => {
-  console.log(allActivities);
   return (
     <div>
       <h2>Month Chart</h2>
