8a5ff78a44e4551756235d48e6c3850f1a160e4b
Shinya Sato <0113shin.s@gmail.com>
2021-07-14 12:33:39 -0700

    style page, intro, body and apply to home and dashboard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,60 +4,73 @@ import signinImg from '../images/dog-sample.jpg'; // dammy img, to be replaced
 
 function Home() {
   return (
-    <div className="home">
+    <div className="page">
       <div className="intro">
-        <h3>Welcom to Pet Paws</h3>
-        <p>
-          We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
-          vets and pet stores for your convenience.
-        </p>
-        <div className="intro__btn">
-          <button className="btn-contained-green">Create free account</button>
+        <div className="intro__wrapper">
+          <div className="intro__body">
+            <div className="intro__text">
+              <h2>Welcom to Pet Paws</h2>
+              <p>
+                Try the best nutrition plan for your furry friend, monitor their daily routine, and locate the best vets
+                and pet shops for your convenience.
+              </p>
+            </div>
+
+            <div className="intro__btn">
+              <button className="btn-contained-white btn-not-fullwidth">Create free account</button>
+            </div>
+          </div>
+
+          <div className="intro__img">
+            <img src={signinImg} alt="member portrait" />
+          </div>
         </div>
       </div>
 
-      <div className="features">
-        <h2>Our Services</h2>
+      <div className="body">
+        <div className="features">
+          <h2>Our Services</h2>
 
-        <div className="features__feature">
-          <h3>Meal and Walk Tracker</h3>
-          <div className="features__feature-img">
-            <img src={signinImg} alt="member portrait" />
+          <div className="features__feature">
+            <h3>Meal and Walk Tracker</h3>
+            <div className="features__feature-img">
+              <img src={signinImg} alt="member portrait" />
+            </div>
+            <p className="features__feature-description">
+              With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be
+              healthy and happy
+            </p>
           </div>
-          <p className="features__feature-description">
-            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
-            and happy
-          </p>
-        </div>
 
-        <div className="features__feature">
-          <h3>Calorie Calculator</h3>
-          <div className="features__feature-img">
-            <img src={signinImg} alt="member portrait" />
+          <div className="features__feature">
+            <h3>Calorie Calculator</h3>
+            <div className="features__feature-img">
+              <img src={signinImg} alt="member portrait" />
+            </div>
+            <p className="features__feature-description">
+              With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be
+              healthy and happy
+            </p>
           </div>
-          <p className="features__feature-description">
-            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
-            and happy
-          </p>
-        </div>
 
-        <div className="features__feature">
-          <h3>Pet Stores Finder</h3>
-          <div className="features__feature-img">
-            <img src={signinImg} alt="member portrait" />
+          <div className="features__feature">
+            <h3>Pet Stores Finder</h3>
+            <div className="features__feature-img">
+              <img src={signinImg} alt="member portrait" />
+            </div>
+            <p className="features__feature-description">
+              With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be
+              healthy and happy
+            </p>
           </div>
-          <p className="features__feature-description">
-            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
-            and happy
-          </p>
+
+          <div className="features__btn">
+            <button className="btn-contained-white btn-not-fullwidth">Unlock All Features</button>
+          </div>
         </div>
 
-        <div className="features__btn">
-          <button className="btn-contained-yellow">Unlock All Features</button>
-        </div>
+        <Team />
       </div>
-
-      <Team />
     </div>
   );
 }
