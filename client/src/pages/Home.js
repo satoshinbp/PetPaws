99f93db9622256ce976f2659ea087fbc82c8cf20
Shinya Sato <0113shin.s@gmail.com>
2021-07-13 20:40:33 -0700

    style features and team for mobile screen

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,62 @@
 import React from 'react';
 import Team from '../components/Team';
+import signinImg from '../images/dog-sample.jpg'; // dammy img, to be replaced
 
 function Home() {
   return (
-    <>
-      <h3>Welcom to Pet Paws</h3>
-      <p>
-        We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
-        vets and pet stores for your convenience.
-      </p>
-      <button className="btn-contained-green">Create free account</button>
-      <div>
-        <h2>Our services</h2>
-        <p>Lorem ipsum dolor sit amet</p>
-        <div>
-          <h3>Feature 1</h3>
-          <p>
-            We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
-            vets and pet stores for your convenience.
-          </p>
-        </div>
-        <div>
-          <h3>Feature 2</h3>
-          <p>
-            We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
-            vets and pet stores for your convenience.
-          </p>
-        </div>
-        <div>
-          <h3>Feature 3</h3>
-          <p>
-            We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
-            vets and pet stores for your convenience.
+    <div className="home">
+      <div className="intro">
+        <h3>Welcom to Pet Paws</h3>
+        <p>
+          We recommend the best nutrition plan for you furry friends, monitor their daily routine, and locate the best
+          vets and pet stores for your convenience.
+        </p>
+        <button className="btn-contained-green">Create free account</button>
+      </div>
+
+      <div className="features">
+        <h2>Our Services</h2>
+
+        <div className="features__feature">
+          <h3>Meal and Walk Tracker</h3>
+          <div className="features__feature-img">
+            <img src={signinImg} alt="member portrait" />
+          </div>
+          <p className="features__feature-description">
+            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
+            and happy
           </p>
         </div>
 
-        <button>UNLOCK ALL FEATURES</button>
+        <div className="features__feature">
+          <h3>Calorie Calculator</h3>
+          <div className="features__feature-img">
+            <img src={signinImg} alt="member portrait" />
+          </div>
+          <p className="features__feature-description">
+            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
+            and happy
+          </p>
+        </div>
+
+        <div className="features__feature">
+          <h3>Pet Stores Finder</h3>
+          <div className="features__feature-img">
+            <img src={signinImg} alt="member portrait" />
+          </div>
+          <p className="features__feature-description">
+            With these 2 trackers you can monitor your pet’s health and know for sure that it gets enough to be healthy
+            and happy
+          </p>
+        </div>
+
+        <div className="features__btn">
+          <button className="btn-contained-yellow">Unlock All Features</button>
+        </div>
       </div>
+
       <Team />
-    </>
+    </div>
   );
 }
 
