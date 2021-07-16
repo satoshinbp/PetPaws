ad107d913fc6f7fcd974714c49f83bb04976d189
Shinya Sato <0113shin.s@gmail.com>
2021-07-15 19:17:24 -0700

    style contact mobile/#156

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,50 +1,14 @@
-import storesVetsFinderIcon from '../images/stores-vets-finder.svg'; // dammy img, to be replaced
+import ContactIntro from '../components/intros/Contact';
+import ContactForm from '../components/forms/Contact';
 
 export default function Contact() {
   return (
     <>
-      <div className="intro">
-        <div className="intro__wrapper">
-          <div className="intro__body">
-            <div className="intro__text">
-              <h2>Ask us a question!</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, assumenda asperiores sunt optio
-                debitis totam repellat soluta voluptas atque recusandae.
-              </p>
-            </div>
-
-            <div className="intro__btn">
-              <button className="btn-contained-white btn-not-fullwidth">Create free account</button>
-            </div>
-          </div>
-
-          <div className="intro__img">
-            <img src={storesVetsFinderIcon} alt="member portrait" />
-          </div>
-        </div>
-      </div>
+      <ContactIntro />
 
       <div className="body">
         <div className="body__wrapper">
-          <form>
-            <div>
-              <label htmlFor="">Your name</label>
-              <br />
-              <input type="text" />
-            </div>
-            <div>
-              <label htmlFor="">Your email</label>
-              <br />
-              <input type="text" />
-            </div>
-            <div>
-              <label htmlFor="">Your message</label>
-              <br />
-              <textarea />
-            </div>
-            <input type="submit" value="Send" />
-          </form>
+          <ContactForm />
         </div>
       </div>
     </>
