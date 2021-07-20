5ef26a1d9dc91d467001645f599b6362e0135567
Shinya Sato <0113shin.s@gmail.com>
2021-07-20 15:41:20 -0700

    style calc intro

--- client/src/components/intros/CalculatorRegisteredUser.js
+++ client/src/components/intros/CalculatorRegisteredUser.js
@@ -1,21 +1,27 @@
-import storesVetsFinderIcon from '../../images/finder.svg'; // dammy img, to be replaced
+import catIcon from '../../images/cat.svg';
 
 export default function CalculatorRegisteredUser() {
   return (
     <div className="intro">
       <div className="wrapper">
-        <div className="intro__body">
-          <div className="intro__text">
-            <h2>Calorie Calculator</h2>
-            <p>
-              Try the best nutrition plan for your furry friend, monitor their daily routine, and locate the best vets
-              and pet shops for your convenience.
-            </p>
+        <div>
+          <h2 className="intro__title">Calorie Calculator</h2>
+          <p>
+            This calculator will help you determine the ideal calorie intake for a pet based on its weight, height, age,
+            breed, activity level and body type!
+          </p>
+
+          <div className="intro__img-area pc-hidden">
+            <img src={catIcon} alt="cat stiting besides cat food" />
+          </div>
+
+          <div className="intro__btn-area">
+            <button className="btn-contained--intro">Create free account</button>
           </div>
         </div>
 
-        <div className="intro__img">
-          <img src={storesVetsFinderIcon} alt="member portrait" />
+        <div className="intro__img-area mb-hidden">
+          <img src={catIcon} alt="cat stiting besides cat food" />
         </div>
       </div>
     </div>
