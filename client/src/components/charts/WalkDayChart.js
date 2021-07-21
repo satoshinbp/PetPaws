3a9756e1e6d8325d6c6443c47802e3f7c9978a31
Shinya Sato <0113shin.s@gmail.com>
2021-07-21 14:48:40 -0700

    refactor summary page basic styling

--- client/src/components/charts/WalkDayChart.js
+++ client/src/components/charts/WalkDayChart.js
@@ -24,39 +24,42 @@ const WalkDayChart = ({ allActivities }) => {
   }, [allActivities, date]);
 
   return (
-    <div className="walk-daily-record">
-      <div className="background-color-wrapper">
-        <div className="info">
-          {/* temporary styling */}
-          <h3>Daily Activities</h3>
-          <label>
-            Choose a date:
-            <input
-              type="date"
-              id="walk-date"
-              name="walk-date"
-              defaultValue={date}
-              required
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </label>
-          {activityForDay.length > 0 && date === activityForDay[0].date ? (
-            <div className="daily-list">
-              <p>{activityForDay[0].date.split('-').join('/')}</p>
-              {activityForDay.map((activity) => (
-                <div className="list-item">
-                  <p key={activity.id}>Activity Name:&nbsp;{activity.name},</p>
-                  <p key={activity.id}>Duratinon:&nbsp;{activity.minute} min,</p>
-                  <p key={activity.id}>
-                    Distance:&nbsp;
-                    {activity.distance ? activity.distance + 'km' : 'no data entered'}
-                  </p>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p>No activity is added</p>
-          )}
+    <div className="bg-secondary-fish">
+      <div className="wrapper">
+        <h2>Daily Activities</h2>
+
+        <div className="bg-secondary-light walk-daily">
+          <div className="info">
+            <label>
+              Choose a date:
+              <input
+                type="date"
+                id="walk-date"
+                name="walk-date"
+                defaultValue={date}
+                required
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </label>
+
+            {activityForDay.length > 0 && date === activityForDay[0].date ? (
+              <div className="daily-list">
+                <p>{activityForDay[0].date.split('-').join('/')}</p>
+                {activityForDay.map((activity) => (
+                  <div className="list-item">
+                    <p key={activity.id}>Activity Name:&nbsp;{activity.name},</p>
+                    <p key={activity.id}>Duratinon:&nbsp;{activity.minute} min,</p>
+                    <p key={activity.id}>
+                      Distance:&nbsp;
+                      {activity.distance ? activity.distance + 'km' : 'no data entered'}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No activity is added</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
