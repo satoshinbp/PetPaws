b712003378d1a9cfa0eddc5aa561e4221ad636e4
Shinya Sato <0113shin.s@gmail.com>
2021-07-22 15:14:44 -0700

    add pet profile image input with style

--- client/src/components/forms/PetProfile.js
+++ client/src/components/forms/PetProfile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import signinImg from '../../images/dog-sample.jpg'; // dammy img, to be replaced
 
 export default function petProfileForm(props) {
   const today = new Date();
@@ -7,16 +6,14 @@ export default function petProfileForm(props) {
   return (
     <form className="basic-form bg-primary-light" onSubmit={(e) => props.handleSubmit(e)}>
       <div className="img-area">
-        <img src={signinImg} alt="" className="profile-img" />
-
-        <label htmlFor="profImage">
-          <input
-            id="profImage"
-            type="file"
-            className="imageInput"
-            onChange={(e) => props.changeImage(e.target.files[0])}
-          />
-          <img id="profImage" src={props.imageURL} />
+        <input
+          id="profImage"
+          type="file"
+          className="profile-img-input"
+          onChange={(e) => props.changeImage(e.target.files[0])}
+        />
+        <label htmlFor="profImage" className="profile-img-label">
+          <img id="profImage" src={props.imageURL} alt="pet-profile" className="profile-img" />
         </label>
       </div>
       <div className="radios-area">
