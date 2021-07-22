66bf0509b695a48041e1ba868457581fce4b779b
Nori <loveandfight2186@yahoo.co.jp>
2021-07-22 15:00:29 -0700

    style: modify sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -31,53 +31,59 @@ export default function Signup() {
   };
 
   return (
-    <div className="signup">
-      <div className="signup_wrapper">
-        <div className="signup_description-area">
-          <div className="signup_description-area_text">
-            <h3>By creating your account,</h3>
-            <p>　You will be able to :</p>
-            <ul>
-              <li>
-                ・<div className="left-align">You can calculate the optimal daily calorie intake for your pet</div>
-              </li>
-              <li>
-                ・<div className="left-align"> You can keep track of your pet's exercise with Activity Tracker</div>
-              </li>
-              <li>
-                ・
-                <div className="left-align">You can also track your pet's diet and calorie intake by Meal Tracker</div>
-              </li>
-            </ul>
+    <>
+      <div className="signup">
+        <div className="intro">
+          <div className="wrapper">
+            <div className="signup_description-area">
+              <h3>By creating your account,</h3>
+              <p>　You will be able to :</p>
+              <ul>
+                <li>
+                  ・<div className="left-align">You can calculate the optimal daily calorie intake for your pet</div>
+                </li>
+                <li>
+                  ・<div className="left-align"> You can keep track of your pet's exercise with Activity Tracker</div>
+                </li>
+                <li>
+                  ・
+                  <div className="left-align">
+                    You can also track your pet's diet and calorie intake by Meal Tracker
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="signup_form">
-          <h2>Sign UP</h2>
-          {error && <div>{error}</div>}
+        <div className="body changeBg">
+          <div className="signup_form">
+            <h2>Sign UP</h2>
+            {error && <div>{error}</div>}
 
-          <form onSubmit={handleSubmit}>
-            <label id="name">Name</label>
-            <input id="name" className="input-md" type="text" ref={nameRef} required />
-            <label id="email">Email</label>
-            <input type="email" className="input-md" ref={emailRef} required />
-            <label id="password">Password</label>
-            <input type="password" className="input-md" ref={passwordRef} required />
-            <label id="password-Confirmation">Password Confirmation</label>
-            <input type="password" className="input-md" ref={passwordConfirmRef} required />
-            <button className="btn-contained" disabled={loading} type="submit">
-              Get Started
-            </button>
-          </form>
-          <div className="signin-link-area">
-            <p>
-              Already have an account?　
-              <Link to="/signin" className="signin-link-area_link">
-                Sign In
-              </Link>
-            </p>
+            <form onSubmit={handleSubmit}>
+              <label id="name">Name</label>
+              <input id="name" className="input-md" type="text" ref={nameRef} required />
+              <label id="email">Email</label>
+              <input type="email" className="input-md" ref={emailRef} required />
+              <label id="password">Password</label>
+              <input type="password" className="input-md" ref={passwordRef} required />
+              <label id="password-Confirmation">Password Confirmation</label>
+              <input type="password" className="input-md" ref={passwordConfirmRef} required />
+              <button className="btn-contained" disabled={loading} type="submit">
+                Get Started
+              </button>
+            </form>
+            <div className="signin-link-area">
+              <p>
+                Already have an account?　
+                <Link to="/signin" className="signin-link-area_link">
+                  Sign In
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
