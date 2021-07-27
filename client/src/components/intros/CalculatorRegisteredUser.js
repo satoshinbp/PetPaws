5e255213f44e458bb124f520e7d270276cf2337c
Shinya Sato <0113shin.s@gmail.com>
2021-07-26 20:14:43 -0700

    take out premium modal from intro component

--- client/src/components/intros/CalculatorRegisteredUser.js
+++ client/src/components/intros/CalculatorRegisteredUser.js
@@ -6,29 +6,31 @@ export default function CalculatorRegisteredUser() {
   const [showPremium, setShowPremium] = useState(false);
 
   return (
-    <div className="intro">
-      <div className="wrapper">
-        <div className="intro-container">
-          <div>
-            <h1 className="intro__title">Calorie Calculator</h1>
-            <p>
-              This calculator will help you determine the ideal calorie intake for a pet based on its weight, height,
-              age, breed, activity level and body type!
-            </p>
+    <>
+      <div className="intro">
+        <div className="wrapper">
+          <div className="intro-container">
+            <div>
+              <h1 className="intro__title">Calorie Calculator</h1>
+              <p>
+                This calculator will help you determine the ideal calorie intake for a pet based on its weight, height,
+                age, breed, activity level and body type!
+              </p>
 
-            <div className="intro__img-area pc-hidden">
+              <div className="intro__img-area pc-hidden">
+                <img src={calculatorIcon} alt="cat stiting besides cat food" />
+              </div>
+
+              <div className="btn-area">
+                <button className="btn-contained--intro" onClick={() => setShowPremium(true)}>
+                  Upgrade to Premium
+                </button>
+              </div>
+            </div>
+
+            <div className="intro__img-area mb-hidden">
               <img src={calculatorIcon} alt="cat stiting besides cat food" />
             </div>
-
-            <div className="btn-area">
-              <button className="btn-contained--intro" onClick={() => setShowPremium(true)}>
-                Upgrade to Premium
-              </button>
-            </div>
-          </div>
-
-          <div className="intro__img-area mb-hidden">
-            <img src={calculatorIcon} alt="cat stiting besides cat food" />
           </div>
         </div>
       </div>
@@ -36,6 +38,6 @@ export default function CalculatorRegisteredUser() {
       <div className={`overlay ${showPremium ? 'isActive' : ''}`}>
         {showPremium && <Premium closeForm={() => setShowPremium(false)} />}
       </div>
-    </div>
+    </>
   );
 }
--- client/src/components/intros/Contact.js
+++ client/src/components/intros/Contact.js
@@ -9,34 +9,36 @@ export default function ContactIntro() {
   const { currentUser } = useAuth();
 
   return (
-    <div className="intro">
-      <div className="wrapper">
-        <div className="intro-container">
-          <div>
-            <h1 className="intro__title">Ask us a question!</h1>
-            <p>Pet Paws Team is always glad to hear your feeback and answer any questions about our services</p>
+    <>
+      <div className="intro">
+        <div className="wrapper">
+          <div className="intro-container">
+            <div>
+              <h1 className="intro__title">Ask us a question!</h1>
+              <p>Pet Paws Team is always glad to hear your feeback and answer any questions about our services</p>
 
-            <div className="intro__img-area pc-hidden">
+              <div className="intro__img-area pc-hidden">
+                <img src={contactIcon} alt="sitting dog with question mark" />
+              </div>
+
+              <div className="btn-area">
+                {currentUser ? (
+                  <button className="btn-contained--intro" onClick={() => setShowPremium(true)}>
+                    Upgrade to Premium
+                  </button>
+                ) : (
+                  <div className="btn-area">
+                    <Link className="btn btn--link btn-contained--intro" to="/signup">
+                      Create Free Account
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="intro__img-area mb-hidden">
               <img src={contactIcon} alt="sitting dog with question mark" />
             </div>
-
-            <div className="btn-area">
-              {currentUser ? (
-                <button className="btn-contained--intro" onClick={() => setShowPremium(true)}>
-                  Upgrade to Premium
-                </button>
-              ) : (
-                <div className="btn-area">
-                  <Link className="btn btn--link btn-contained--intro" to="/signup">
-                    Create Free Account
-                  </Link>
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div className="intro__img-area mb-hidden">
-            <img src={contactIcon} alt="sitting dog with question mark" />
           </div>
         </div>
       </div>
@@ -44,6 +46,6 @@ export default function ContactIntro() {
       <div className={`overlay ${showPremium ? 'isActive' : ''}`}>
         {showPremium && <Premium closeForm={() => setShowPremium(false)} />}
       </div>
-    </div>
+    </>
   );
 }
